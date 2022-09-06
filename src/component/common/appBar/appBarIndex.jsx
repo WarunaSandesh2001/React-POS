da6304a2dad9c7646afc33638a058fd6ed1c4afe
WarunaSandesh2001 <warunasandesh2001@gmail.com>
2022-09-06 18:41:11 +0530

    Updated README File

--- src/component/common/appBar/appBarIndex.jsx
+++ src/component/common/appBar/appBarIndex.jsx
@@ -135,6 +135,7 @@ const ResponsiveAppBar = () => {
                                 <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                             </IconButton>
                         </Tooltip>
+                        {/* ============================================================================= */}
                         <Menu
                             sx={{ mt: '45px' }}
                             id="menu-appbar"
